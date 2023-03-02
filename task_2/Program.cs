a0d123745ffd165255d9f879aedb7351f10c2fb5
Rufat Karimov <ruffat_karimov@mail.ru>
2023-03-02 13:37:25 +0600

    sec task

---
+++ task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] get_random(int n)
+{
+    int[] result = new int [n];
+    for(int i = 0; i < n; i++)
+    {
+    result[i] = new Random(). Next (-20, 20);
+    }
+    return result;
+}
+
+
+int[] array = get_random(6);
+    int sum = 0;
+    int i = 1;
+    for(; i <= array.Length; i = i + 2)
+    {
+        sum += array[i];
+    }
+  
+Console.WriteLine($"[{String.Join(", ", array)},] -->  {sum}");
+
+
+
+
+
