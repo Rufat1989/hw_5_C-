0939a0d9980e4bcb28be320aff25d782fa767a47
Rufat Karimov <ruffat_karimov@mail.ru>
2023-03-02 16:33:23 +0600

    third task

---
+++ task_3/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] get_random(int n)
+{
+    int[] result = new int [n];
+    for(int i = 0; i < n; i++)
+    {
+    result[i] = new Random(). Next (0, 100);
+    }
+    return result;
+}
+
+int[] array = get_random(6);
+    int j = 0;
+    int max = array[j];
+    int min = array[j];
+    
+    foreach(int el in array)
+    {
+        while(el > max ) 
+        {
+            max = el;
+            j ++;
+            
+        }
+        if(el < min)
+            {
+                min = el;
+                
+            j ++;
+             }
+                
+    }
+  
+
+  
+Console.WriteLine($"[{String.Join(", ", array)},] -->  {max - min}");
