ba5e0b787cba57389e7549b047b1da3268ef81e7
Rufat Karimov <ruffat_karimov@mail.ru>
2023-03-02 13:24:51 +0600

    first task

---
+++ task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] get_random(int n)
+{
+    int[] result = new int [n];
+    for(int i = 0; i < n; i++)
+    {
+    result[i] = new Random(). Next (99, 1000);
+    }
+    return result;
+}
+
+
+int[] array = get_random(6);
+    int count = 0;
+    foreach(int el in array)
+    {
+        if(el % 2 == 0) 
+        {
+            count ++;
+        }
+    }
+   
+Console.WriteLine($"[{String.Join(", ", array)},] -->  {count}");
+
